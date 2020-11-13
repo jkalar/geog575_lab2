bd4d013bd7a4d351bd104ae4c44fa9ccb9a8d1f3
jkalar <jkalar@wisc.edu>
2020-11-13 10:41:02 -0600

    x

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@ function setChart(csvData, colorScale){
 	//Code for labeling the bars directly would go here. Label Y axis instead.	
 	
 	var chartTitle = chart.append("text")
-        .attr("x", 76)
+        .attr("x", 80)
         .attr("y", 25)
         .attr("class", "chartTitle")
         .text("Percent of eligible voters who voted in the selected year.");
